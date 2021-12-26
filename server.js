fd9609a1cc4ebe5b335d17fa50bdaa7f5d59c043
ByteSizeDev <67930601+ByteSizeDev@users.noreply.github.com>
2021-12-25 23:45:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ const client = new tmi.Client({
         username: process.env.TWITCH_BOT_USERNAME,
         password: process.env.TWITCH_OAUTH_TOKEN
     },
-    channels: [ 'bytesized_' ]
+    channels: [ 'grfnn' ]
 });
 
 client.connect();
